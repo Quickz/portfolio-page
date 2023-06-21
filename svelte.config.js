e0499220c2b244d9c062c1433bacbef35ae2f3f9
Quickz <alexserg@inbox.lv>
2023-06-21 19:13:29 +0300

    updated a comment

--- svelte.config.js
+++ svelte.config.js
@@ -8,9 +8,8 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		// Thing responsible for the geenrated output
+		// Reference: https://kit.svelte.dev/docs/adapter-static
 		adapter: adapter({
 			fallback: '200.html'
 		})
